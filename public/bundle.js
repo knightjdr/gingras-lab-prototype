3088122cca4a558a2c002a10ba5b9bfe6360c89b
James Knight <knightjdr@gmail.com>
2019-08-21 08:48:09 -0400

    Name functions

--- public/bundle.js
+++ public/bundle.js
@@ -1,43 +1,43 @@
-const isVisible = function (element) {
+function isVisible (element) {
   if (!element) {
     return false;
   }
-  const rect = element.getBoundingClientRect();
+  var rect = element.getBoundingClientRect();
   return Boolean(
     window.getComputedStyle(element).getPropertyValue('opacity') > 0
     && (rect.height || rect.width)
   );
-};
+}
 
-const clickListener = function (e) {
-  const element = document.querySelector('.navbar__menu');
-  const outside = !element.contains(e.target) && isVisible(element);
+function clickListener (e) {
+  var element = document.querySelector('.navbar__menu');
+  var outside = !element.contains(e.target) && isVisible(element);
   if (outside) {
     removeListeners();
     element.classList.remove('open');
   }
-};
+}
 
-const escListener = function (e) {
-  const element = document.querySelector('.navbar__menu');
+function escListener (e) {
+  var element = document.querySelector('.navbar__menu');
   if (e.key === 'Escape' && isVisible(element)) {
     removeListeners();
     element.classList.remove('open');
   }
-};
+}
 
-const addListeners = function () {
+function addListeners () {
   window.addEventListener('click', clickListener);
   window.addEventListener('keydown', escListener);
-};
+}
 
-const removeListeners = function () {
+function removeListeners () {
   window.removeEventListener('click', clickListener);
   window.removeEventListener('keydown', escListener);
-};
+}
 
 document.querySelector('.navbar__menu-button').addEventListener('click', function () {
-  const menu = document.querySelector('.navbar__menu');
+  var menu = document.querySelector('.navbar__menu');
   if (menu.classList.contains('open')) {
     removeListeners();
     menu.classList.remove('open');
