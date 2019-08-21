e5e877a68c7539e99c43682013db212278965117
James <jknight@lunenfeld.ca>
2019-08-21 16:58:31 -0400

    Add image generator

--- README.md
+++ README.md
@@ -12,3 +12,18 @@ npm run deploy
 ```
 npm run production
 ```
+
+## Convert images
+
+Convert images to WebP and reduce quality of JPEGs.
+
+1. Create image at 3x the required dimensions and place in `src/images/unprocessed`.
+
+2. Move into directory and convert images
+```
+cd src/images
+node index.js
+```
+
+3. JPEGs and WebP at 1x, 2x and 3x scale will be in the `processed` folder. The 3x images
+correspond to the dimensions of the original image.
---
+++ src/images/index.js
@@ -0,0 +1,21 @@
+const copyImages = require('./copy-images');
+const convertWebp = require('./convert-webp');
+const readImages = require('./read-images');
+const reduceJPG = require('./reduce-jpg');
+const scaleImages = require('./scale-images');
+
+const quality = 70;
+
+const convertImages = async () => {
+  try {
+    const files = await readImages('./unprocessed', ['.jpg', '.jpeg', '.png']);
+    await copyImages(files);
+    await scaleImages(files);
+    await convertWebp(quality);
+    await reduceJPG(quality);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+convertImages();
